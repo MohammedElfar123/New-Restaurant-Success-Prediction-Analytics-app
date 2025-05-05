8d1da176ba68b7c8fd4a508b0137759e254ec16e
Mohamed Mahmoud <123865756+MohammedElfar123@users.noreply.github.com>
2025-05-05 22:27:35 +0300

    Update Main_Streamlit .py

--- Main_Streamlit .py
+++ Main_Streamlit .py
@@ -7,7 +7,7 @@ import plotly.express as px
 
 # Load the trained model and data
 model_path = "/Users/mohammedmahmood/Desktop/Grad_project/Resturant_Succsess_prediction.sav"
-data_path = "/Users/mohammedmahmood/Desktop/Grad_project/Cleaned_data2.pkl"
+data_path = "Cleaned_data2.pkl"
 
 with open(model_path, "rb") as file:
     Final_Model = pickle.load(file)
